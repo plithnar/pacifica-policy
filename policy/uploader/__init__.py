c22b8b883563b9d23e21c21c03dd7c22e96b34a4
David Brown <dmlb2000@gmail.com>
2016-08-28 16:50:36 -0700

    clean this up and move things to better locations

--- policy/uploader/__init__.py
+++ policy/uploader/__init__.py
@@ -1,5 +1,14 @@
 #!/usr/bin/python
 """
-CherryPy Uploader object classes
+CherryPy Uploader Policy object class
 """
-from policy import UploaderPolicy
+
+# pylint: disable=too-few-public-methods
+class UploaderPolicy(object):
+    """
+    CherryPy root object class
+
+    not exposed by default the base objects are exposed
+    """
+    exposed = True
+# pylint: enable=too-few-public-methods
