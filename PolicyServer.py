0ed8f6e514931f2906e13533766df1b4e7d9f7ed
David Brown <dmlb2000@gmail.com>
2017-03-03 12:03:23 -0800

    fix #38 add print to stderr to allow debugging to happen (#39)
    This adds print statements to print the default error content to
    stderr which should dump out in the logs for uwsgi

--- PolicyServer.py
+++ PolicyServer.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python
 """This is the main policy server script."""
+from __future__ import print_function
+import sys
 from json import dumps
 import cherrypy
 from policy.root import Root
@@ -9,12 +11,15 @@ from policy import try_meta_connect
 def error_page_default(**kwargs):
     """The default error page should always enforce json."""
     cherrypy.response.headers['Content-Type'] = 'application/json'
-    return dumps({
+    json_str = dumps({
+        'postdata': cherrypy.request.body.read(),
         'status': kwargs['status'],
         'message': kwargs['message'],
         'traceback': kwargs['traceback'],
         'version': kwargs['version']
-    })
+    }, sort_keys=True, indent=4)
+    print(json_str, file=sys.stderr)
+    return json_str
 
 
 def main():
