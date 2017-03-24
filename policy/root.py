52092bd4e159c56e2e1ddf59a5458fd176091d5d
David Brown <dmlb2000@gmail.com>
2017-03-23 18:06:03 -0700

    fix #4 call try_meta_connect in root cherrypy constructor (#5)
    * fix #4 call try_meta_connect in root cherrypy constructor
    
    Seems like the constructor wasn't calling try_meta_connect to see
    if the metadata services are up yet. So this was exiting very
    quickly on startup.
    
    * fix some of the pre-commit issues

--- policy/root.py
+++ policy/root.py
@@ -3,6 +3,7 @@
 from policy.uploader.rest import UploaderPolicy
 from policy.status.rest import StatusPolicy
 from policy.ingest.rest import IngestPolicy
+from policy import try_meta_connect
 
 
 # pylint: disable=too-few-public-methods
@@ -17,6 +18,7 @@ class Root(object):
 
     def __init__(self):
         """Create the local objects we need."""
+        try_meta_connect()
         self.uploader = UploaderPolicy()
         self.status = StatusPolicy()
         self.ingest = IngestPolicy()
