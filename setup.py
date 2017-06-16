7928ffb19dc10dab677b6a598005f47cdb303efc
David Brown <dmlb2000@gmail.com>
2017-06-15 20:17:41 -0700

    add setup.py script to allow for policy to be installed via pip (#15)

---
+++ setup.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python
+"""Setup and install the policy."""
+from pip.req import parse_requirements
+from setuptools import setup
+
+# parse_requirements() returns generator of pip.req.InstallRequirement objects
+INSTALL_REQS = parse_requirements('requirements.txt', session='hack')
+
+setup(name='PacificaPolicy',
+      version='1.0',
+      description='Pacifica Policy',
+      author='David Brown',
+      author_email='david.brown@pnnl.gov',
+      packages=['policy'],
+      scripts=['PolicyServer.py'],
+      install_requires=[str(ir.req) for ir in INSTALL_REQS])
--- travis/test-deploy.sh
+++ travis/test-deploy.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-docker build -t pacifica/policy .
+pip install .
