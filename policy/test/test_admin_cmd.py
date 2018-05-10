c6ec4637961f9a4fea686c108380cd865e306a49
David Brown <dmlb2000@gmail.com>
2018-05-10 14:03:41 -0700

    try using smaller number of objects per page

--- policy/test/test_admin_cmd.py
+++ policy/test/test_admin_cmd.py
@@ -12,9 +12,9 @@ class TestAdminCMD(TestCase):
 
     def test_default_search_sync(self):
         """Test the data release subcommand."""
-        main('searchsync')
+        main('searchsync', '--objects-per-page', '4')
         SearchRender.obj_cache = LimitedSizeDict(size_limit=1)
-        main('searchsync')
+        main('searchsync', '--objects-per-page', '4')
         resp = requests.get('http://localhost:9200/pacifica_search/_stats')
         self.assertEqual(resp.status_code, 200)
         self.assertEqual(
