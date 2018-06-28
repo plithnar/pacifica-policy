900ab887f4825cb55d823c805f61d7e9fe6c0a47
David Brown <dmlb2000@gmail.com>
2018-06-28 14:01:41 -0700

    hopefully this makes it better

--- policy/test/test_admin_cmd.py
+++ policy/test/test_admin_cmd.py
@@ -13,7 +13,7 @@ class TestAdminCMD(TestCase):
     def test_default_search_sync(self):
         """Test the data release subcommand."""
         main('searchsync', '--objects-per-page', '4', '--threads', '1')
-        SearchRender.obj_cache = LimitedSizeDict(size_limit=200)
+        SearchRender.obj_cache = LimitedSizeDict(size_limit=4000)
         main('searchsync', '--objects-per-page', '4', '--threads', '1')
         resp = requests.get('http://localhost:9200/pacifica_search/_stats')
         self.assertEqual(resp.status_code, 200)
