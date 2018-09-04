98b30d5085bd4187375a0f50424160c9bce8b375
David Brown <dmlb2000@gmail.com>
2018-09-04 11:45:41 -0700

    fix #49 make data release actually touch data releasing (#50)
    * fix #49 make data release actually touch data releasing
    
    * sets are builtin for a while what am I doing
    
    * add test to run data_release on transactions
    
    * add transaction we can check that goes into transaction release table
    
    * get the right key
    
    * fix data release to test properly
    
    * add appropriate admin user to do the releasing

--- policy/data_release.py
+++ policy/data_release.py
@@ -2,6 +2,7 @@
 # -*- coding: utf-8 -*-
 """Data release policy for command line tools."""
 from __future__ import print_function
+from os import getenv
 from datetime import datetime
 from json import dumps
 from dateutil import parser
@@ -20,9 +21,45 @@ VALID_KEYWORDS = [
 ]
 
 
-def relavent_data_release_objs(time_ago, orm_obj, date_key):
+def relavent_data_release_objs(time_ago, orm_obj):
+    """Query proposals or transactions that has gone past their suspense date."""
+    trans_objs = set()
+    suspense_args = {
+        'suspense_date': 0,
+        'suspense_date_0': (
+            datetime.now() - time_ago
+        ).replace(microsecond=0).isoformat(),
+        'suspense_date_1': datetime.now().replace(microsecond=0).isoformat(),
+        'suspense_date_operator': 'between'
+    }
+    resp = requests.get(
+        text_type('{base_url}/{orm_obj}?{args}').format(
+            base_url=METADATA_ENDPOINT,
+            orm_obj=orm_obj,
+            args='&'.join(['{}={}'.format(key, value)
+                           for key, value in suspense_args.items()])
+        )
+    )
+    if orm_obj == 'proposals':
+        for prop_obj in resp.json():
+            prop_id = prop_obj['_id']
+            resp = requests.get(
+                text_type('{base_url}/transactions?proposal_id={prop_id}').format(
+                    base_url=METADATA_ENDPOINT,
+                    prop_id=prop_id
+                )
+            )
+            for trans_obj in resp.json():
+                trans_objs.add(trans_obj['_id'])
+    else:
+        for trans_obj in resp.json():
+            trans_objs.add(trans_obj['_id'])
+    return trans_objs
+
+
+def relavent_suspense_date_objs(time_ago, orm_obj, date_key):
     """generate a list of relavent orm_objs saving date_key."""
-    objs = {}
+    objs = set()
     for time_field in ['updated', 'created']:
         resp = requests.get(
             text_type('{base_url}/{orm_obj}?{time_field}={epoch}&{time_field}_operator=gt').format(
@@ -35,14 +72,14 @@ def relavent_data_release_objs(time_ago, orm_obj, date_key):
             )
         )
         for chk_obj in resp.json():
-            if chk_obj['_id'] not in objs.keys() and chk_obj.get(date_key, False):
-                objs[chk_obj['_id']] = chk_obj[date_key]
+            if chk_obj.get(date_key, False):
+                objs.add((chk_obj['_id'], chk_obj[date_key]))
     return objs
 
 
-def update_data_release_objs(objs, time_after, orm_obj):
+def update_suspense_date_objs(objs, time_after, orm_obj):
     """update the list of objs given date_key adding time_after."""
-    for obj_id, obj_date_key in objs.items():
+    for obj_id, obj_date_key in objs:
         resp = requests.post(
             text_type('{base_url}/{orm_obj}?_id={obj_id}').format(
                 base_url=METADATA_ENDPOINT,
@@ -62,6 +99,31 @@ def update_data_release_objs(objs, time_after, orm_obj):
         assert resp.status_code == 200
 
 
+def update_data_release(objs):
+    """Add objs transactions to the released transactions table."""
+    for trans_id in objs:
+        resp = requests.get(
+            text_type(
+                '{base_url}/transaction_release?transaction={trans_id}'
+            ).format(
+                base_url=METADATA_ENDPOINT,
+                trans_id=trans_id
+            )
+        )
+        if resp.status_code == 200 and resp.json():
+            continue
+        resp = requests.put(
+            text_type(
+                '{base_url}/transaction_release').format(base_url=METADATA_ENDPOINT),
+            data=dumps({
+                'authorized_person': getenv('ADMIN_USER_ID', -1),
+                'transaction': trans_id
+            }),
+            headers={'content-type': 'application/json'}
+        )
+        assert resp.status_code == 200
+
+
 def data_release(args):
     """
     Data release main subcommand.
@@ -70,7 +132,13 @@ def data_release(args):
     args.time_ago. If the objects args.keyword is set to something
     calculate the suspense date as args.time_after the keyword date.
     Then save the object back to the metadata server.
+
+    The follow on task is to use orm_obj to calculate the released
+    data based on the set suspense dates and add that released data
+    to the transaction_release table.
     """
     orm_obj, date_key = args.keyword.split('.')
-    objs = relavent_data_release_objs(args.time_ago, orm_obj, date_key)
-    update_data_release_objs(objs, args.time_after, orm_obj)
+    objs = relavent_suspense_date_objs(args.time_ago, orm_obj, date_key)
+    update_suspense_date_objs(objs, args.time_after, orm_obj)
+    trans_objs = relavent_data_release_objs(args.time_ago, orm_obj)
+    update_data_release(trans_objs)
--- policy/test/test_admin_cmd.py
+++ policy/test/test_admin_cmd.py
@@ -9,9 +9,25 @@ from ..admin_cmd import main
 class TestAdminCMD(TestCase):
     """Test the admin command line tools."""
 
+    def test_trans_data_release(self):
+        """Test transaction data release."""
+        main('data_release', '--keyword', 'transactions.created',
+             '--time-after', '365 days after')
+        resp = requests.get('http://localhost:8121/transactions?_id=68')
+        self.assertEqual(resp.status_code, 200)
+        self.assertEqual(resp.json()[0]['suspense_date'], '2018-07-10')
+        resp = requests.get(
+            'http://localhost:8121/transaction_release?transaction=68')
+        self.assertEqual(resp.status_code, 200)
+        self.assertEqual(resp.json()[0]['transaction'], 68)
+
     def test_default_data_release(self):
         """Test the data release subcommand."""
         main('data_release', '--time-after', '365 days after')
         resp = requests.get('http://localhost:8121/proposals?_id=1234a')
         self.assertEqual(resp.status_code, 200)
         self.assertEqual(resp.json()[0]['suspense_date'], '2018-09-29')
+        resp = requests.get(
+            'http://localhost:8121/transaction_release?transaction=1234')
+        self.assertEqual(resp.status_code, 200)
+        self.assertEqual(resp.json()[0]['transaction'], 1234)
