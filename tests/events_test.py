b3951131c660f61281adf6b41be0178650ed8706
David Brown <dmlb2000@gmail.com>
2019-05-23 18:04:03 -0700

    Fix #85 Verify Policy Events are Type Ingest (#87)
    This verifies that the policy events are of type ingest and not
    something else. This prevents a bunch of unwanted stackdumps in
    the logs.
    
    Signed-off-by: David Brown <dmlb2000@gmail.com>

--- tests/events_test.py
+++ tests/events_test.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 """Test the events policy."""
 from os.path import join
-from json import loads
+from json import loads, dumps
 from cherrypy.test import helper
 from common_test import CommonCPSetup
 
@@ -22,3 +22,18 @@ class TestEventsPolicy(helper.CPWebCase, CommonCPSetup):
         self.assertFalse(ret_data is None)
         self.assertTrue('status' in ret_data)
         self.assertEqual(ret_data['status'], 'success')
+
+    def test_bad_events_query(self):
+        """Test posting the queries."""
+        invalid_query = {'some': {'random': 'data'}, 'in': ['a', 'hash']}
+        self.getPage('/events/dmlb2001',
+                     self.headers +
+                     [('Content-Length', str(len(dumps(invalid_query))))],
+                     'POST',
+                     dumps(invalid_query))
+        self.assertStatus('412 Precondition Failed')
+        self.assertHeader('Content-Type', 'application/json')
+        ret_data = loads(self.body.decode('UTF-8'))
+        self.assertFalse(ret_data is None)
+        self.assertTrue('message' in ret_data)
+        self.assertEqual(ret_data['message'], 'Precondition Failed: Invalid eventType for Not Present')
